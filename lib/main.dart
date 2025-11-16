4a3a5899e77ca83213b97e6cc98b59f00bfc0d39
NeoRise456 <defneorise@gmail.com>
2025-11-15 20:30:38 -0500

    feat(firebase_integration): add Firebase configuration and initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:ergovision/shared/services/auth_service.dart';
 import 'package:ergovision/shared/views/sign-in.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await AuthService.instance.loadToken();
 
   runApp(const MyApp());
